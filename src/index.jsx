583f38eabf2d960132d189acacb2338785fe125e
mellunar <melfer100@gmail.com>
2021-10-10 18:38:25 -0300

    portfolio

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { HashRouter } from 'react-router-dom';
 import Root from './Pages/Root';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Root />
+    <HashRouter>
+      <Root />
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 )
